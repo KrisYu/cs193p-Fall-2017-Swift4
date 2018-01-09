d9e0490b5b1839e369415a26526880ae30eca154
KrisYu <abcyuxue123@126.com>
2018-01-09 11:17:21 -0800

    Lecture 3 & 4

--- code/Concentration_l4/Concentration/Concentration.swift
+++ code/Concentration_l4/Concentration/Concentration.swift
@@ -8,7 +8,7 @@
 
 import Foundation
 
-class Concentration {
+struct Concentration {
     
     private(set) var cards = [Card]()
     
@@ -28,7 +28,8 @@ class Concentration {
         }
     }
     
-    func chosenCard(at index: Int) {
+    // this mutating keyword used because now Concentration is a struct
+    mutating func chosenCard(at index: Int) {
         // protects the index out of boundaray error
         assert(cards.indices.contains(index), "Concentration.chosenCard(at: (\(index)): chosen index not in the cards")
         if !cards[index].isMatched {
--- notes/lecture_4.md
+++ notes/lecture_4.md
@@ -7,3 +7,4 @@ Paul Hegarty continues his coverage of Swift.
 - 写了一个Collection extension，collection包本身是协议，是array,dictionary,set遵循的协议，感觉协议的好处是我们遵循协议，而不用继承，因为有的时候继承不是特别合适，协议就可以跳出来，protocol programming也是一种编程范式吧
 - closure: 虽说closure就是匿名函数，但是要注意它的close周围变量的特性，同时closure可以各种简写
 - NSAttributedString: 就是有属性的String, NS一般是OC的类
+- struct: 把concentration-game logic变成了struct，struct改变自身的函数需要加上mutating
